5426df8bfbc24a5eddd40bf64467ea91ba03b052
Ryan Quoc Le <quoc.letranminh@gmail.com>
2020-05-20 09:15:16 +0700

    commit

---
+++ src/cores/genetic.ts
@@ -0,0 +1,19 @@
+export class Genetic {
+    public population: any;
+    public random: any;
+    public mutate: any;
+    public crossover: any;
+    public fitness: any;
+    private config: any;
+
+    constructor(config) {
+        this.config = config;
+    }
+
+    public start() {
+        this.population = [];
+
+        for
+
+    }
+}
